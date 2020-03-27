26265f31b15f5eaee1948b905cf8769564436712
unknown <jacob.e.england@gmail.com>
2020-03-27 10:18:16 -0500

    Added login form, axiosWithAuth, and privateRoute

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+  const [login, setLogin] = useState({username: '', password: ''});
+  const [error, setError] = useState('');
+  const handleChange = (event) => {
+    setLogin({
+        ...login,
+        [event.target.name]: event.target.value,
+    })
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axiosWithAuth()
+        .post('/api/login', login)
+        .then((response) => {
+            console.log(response);
+            localStorage.setItem('token', response.data.payload);
+            setError('');
+        })
+        .catch((error) => setError(error.response.data.error));
+    setLogin({username: '', password: ''});
+  }
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
-  );
+    <form onSubmit={handleSubmit}>
+        <div className='formRow'>
+            <label htmlFor='username'>Username:</label>
+            <input type='text' id='username' name='username' value={login.username} onChange={handleChange}/>
+        </div>
+        <div className='formRow'>
+            <label htmlFor='password'>Password:</label>
+            <input type='password' id='password' name='password' value={login.password} onChange={handleChange}/>
+        </div>
+        <button type='submit'>Login</button>
+        {error ? <p className='error'>{error}</p> : undefined }
+    </form>
+);
 };
 
 export default Login;
